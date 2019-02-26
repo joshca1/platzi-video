65af886a6b73d5e8943fcb3bb61c6d9775ef6578
unknown <joseacm1989@gmail.com>
2019-02-25 21:19:16 -0400

    dd

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 //import PropTypes from 'prop-types'
 import Media from './Media'
-import '../App.css'
 class Playlist extends Component {
   render() {
     const playlist = this.props.data.categories
