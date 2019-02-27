2274bdbdbe797358b3eb887e2ec2c977bfc74722
joshca1santi <jose@santi.com.ve>
2019-02-27 10:27:21 -0430

    adding modal

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import Sidebar from './components/sidebar/sidebar'
 import Header from './components/header/header'
 import './App.css'
 import data from './dummi-data/data.json'
-
+import ModalContainer from './widget/containers/modal'
+import Modal from './components/modal/modal'
 class App extends Component {
+  state = {
+    modalVisible: false
+  }
+  handleCloseModal = (event) => {
+    this.setState({
+      modalVisible: false
+    })
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <Sidebar className="sidebar" />
         <Playlist data={data} />
+        {this.state.modalVisible &&
+          <ModalContainer>
+            <Modal handleClose={this.handleCloseModal}>
+              SOy  YO UN Modal
+          </Modal>
+          </ModalContainer>
+        }
       </div>
     )
   }
